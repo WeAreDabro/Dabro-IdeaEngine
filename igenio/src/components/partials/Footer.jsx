ec28a39f56797c8d350878b1bc0867bcadaa7285
Darren Nesbitt <darrencnesbitt@gmail.com>
2017-09-21 09:14:49 -0400

    header footer partials

---
+++ igenio/src/components/partials/Footer.jsx
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+
+class Footer extends Component {
+  render() {
+    return (
+      <p>Created by DABRO - WDI - NEW YORK</p>
+    );
+  };
+}
+
+export default Footer;
