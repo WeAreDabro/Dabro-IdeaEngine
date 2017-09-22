0f83050590373afe0f1879a18d7f20b596d8883a
Darren Nesbitt <darrencnesbitt@gmail.com>
2017-09-22 16:53:09 -0400

    added axios call to our server

--- igenio/src/components/SingleTweet.jsx
+++ igenio/src/components/SingleTweet.jsx
@@ -1,11 +1,30 @@
 import React, { Component } from 'react';
-
+import Twit from 'twit';
+import Config from '../config';
+import axios from 'axios';
 
 class SingleTweet extends Component {
+ constructor() {
+  super();
+  this.state = {
+    tweets: []
+  };
+ }
+
+  componentDidMount() {
+    axios.get('/api/tweets')
+    .then(res => {
+      console.log(res.data.statuses)
+    })
+
+
+}
+
   render() {
     return (
       <div>
-        <h2> this is a single tweet from the Twitter Api</h2>
+
+        <h2> thiz is a single tweet from the Twitter Api</h2>
       </div>
     )
   }
